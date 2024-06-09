f6f9d31a9d65ac514c52df2978000e05880acd78
1Kill2Steal <171241044+1Git2Clone@users.noreply.github.com>
2024-06-09 16:43:46 +0300

    Updated parsing to change ints to floats

--- CurrencyConversion.py
+++ CurrencyConversion.py
@@ -1,25 +1,22 @@
-def valid_currency(value: str) -> bool:
-    try:
-        float(value)
-        split = value.split('.')
-        if len(split[1]) > 2:
-            return False;
-    except:
-        try:
-            int(value)
-        except:
-            return False;
-
-    return True;
-
 def main():
+    as_float: float
     while True:
         amount: str = input("Enter amount: ")
-        if valid_currency(amount):
-            break
-        print("Please enter an integer or a decimal value with up to 2 floating points.")
-
-    parsed_amount = float(amount)
+        if not amount:
+            continue
+        try:
+            split = amount.split('.')
+            if len(split) == 2 and len(split[1]) > 2:
+                continue
+            as_float = float(amount)
+            break;
+        except ValueError:
+            try:
+                as_float = float(amount + ".0")
+                break
+            except ValueError:
+                print("Please enter an integer or a decimal value with up to 2 floating points.")
+                continue
 
 if __name__ == "__main__":
     main()
